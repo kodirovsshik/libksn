9c0785ca4d03699f3e7ed0586ac5e4bedfcdf8a4
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2021-02-02 04:50:12 +0300

    [Stuff] ksn_dynamic_assert macro fix

--- ksn/include/ksn/stuff.hpp
+++ ksn/include/ksn/stuff.hpp
@@ -76,7 +76,7 @@ const void* memnotchr(const void* block, uint8_t value, size_t length);
 
 
 void dynamic_assert(int nonzero, const char* expr, const char* msg, int line, const char* file, ...);
-#define ksn_dynamic_assert(true_expr, msg) ksn::dynamic_assert(!!(true_expr), #true_expr, msg, __LINE__, __FILE__, __VA_ARGS__);
+#define ksn_dynamic_assert(true_expr, msg, ...) ksn::dynamic_assert(!!(true_expr), #true_expr, msg, __LINE__, __FILE__, __VA_ARGS__);
 
 
 
