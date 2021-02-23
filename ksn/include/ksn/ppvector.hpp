19313b57df8d5118357422cb1c7de3124935e7d9
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2021-02-23 16:15:46 +0300

    [ppvector] Added destructor (yeah, true), some type check magic

--- ksn/include/ksn/ppvector.hpp
+++ ksn/include/ksn/ppvector.hpp
@@ -4,8 +4,12 @@
 
 
 #include <ksn/ksn.hpp>
+#include <ksn/metapr.hpp>
+
 #include <stdlib.h>
 #include <type_traits>
+#include <string.h>
+
 
 
 _KSN_BEGIN
@@ -13,20 +17,24 @@ _KSN_BEGIN
 //Poor programmer's vector
 //std::vector class for trivial types (only!!)
 
-template<typename T>
+template<typename T> requires(!std::is_const_v<T> && !std::is_volatile_v<T> && std::is_trivial_v<T>)
 struct ppvector
 {
-#ifndef _KSN_PPVECTOR_NO_TRIVIAL_TYPE_CHECK_
-	static_assert(std::is_trivial_v<T>, "ksn::ppvector only works with trivial types. #define _KSN_PPVECTOR_NO_TRIVIAL_TYPE_CHECK_ to suppress this warning");
-#endif // !_KSN_PPVECTOR_NO_TRIVIAL_TYPE_CHECK_
+//#ifndef _KSN_PPVECTOR_NO_TRIVIAL_TYPE_CHECK_
+//	static_assert(std::is_trivial_v<T>, "ksn::ppvector only works with trivial types. #define _KSN_PPVECTOR_NO_TRIVIAL_TYPE_CHECK_ to suppress this warning");
+//#endif // !_KSN_PPVECTOR_NO_TRIVIAL_TYPE_CHECK_
 
 	size_t m_count, m_capacity;
 	T* m_buffer;
 
+	~ppvector() noexcept
+	{
+		this->free();
+	}
 	ppvector() noexcept
 	{
 		this->m_buffer = nullptr;
-		this->m_count = this->m_capacity = 0;
+		this->m_capacity = this->m_count = 0;
 	}
 	ppvector(const ppvector<T>& r) noexcept
 	{
@@ -34,7 +42,7 @@ struct ppvector
 		if (this->reserve(r.m_capacity))
 		{
 			memcpy(this->m_buffer, r.m_buffer, r.m_count * sizeof(T));
-			//this->m_capacity = r.m_capacity;
+			//this->m_capacity = r.m_capacity; //Done by this->reserve(...)
 			this->m_count = r.m_count;
 		}
 	}
@@ -45,8 +53,34 @@ struct ppvector
 		this->m_count = r.m_count;
 		r.ppvector<T>::ppvector();
 	}
+	ppvector(const std::initializer_list<T>& list) noexcept
+	{
+		this->ppvector<T>::ppvector();
+		if (this->reserve(list.size()))
+		{
+			memcpy(this->m_buffer, list.begin(), list.size() * sizeof(T));
+			this->m_count = list.size();
+		}
+	}
 
-	template<std::convertible_to<T> U>
+	ppvector& operator=(const ppvector<T>& r) noexcept
+	{
+		if (this->reserve(r.m_count))
+		{
+			this->m_count = r.m_count;
+			memcpy(this->m_buffer, r.m_buffer, sizeof(T) * r.m_count);
+		}
+		return *this;
+	}
+	ppvector& operator=(ppvector<T>&& r) noexcept
+	{
+		std::swap(this->m_buffer, r.m_buffer);
+		std::swap(this->m_capacity, r.m_capacity);
+		std::swap(this->m_count, r.m_count);
+		return *this;
+	}
+
+	template<typename U> requires(std::convertible_to<U, T>)
 	ppvector(const ppvector<U>& r) noexcept
 	{
 		this->ppvector<T>::ppvector();
@@ -58,7 +92,7 @@ struct ppvector
 			}
 		}
 	}
-	template<std::convertible_to<T> U>
+	template<typename U> requires(std::convertible_to<U, T>)
 	ppvector(ppvector<U>&& r) noexcept
 	{
 		if constexpr (sizeof(T) <= sizeof(U))
@@ -72,9 +106,21 @@ struct ppvector
 			this->ppvector<T>::ppvector();
 			if (!this->reserve(r.m_capacity)) return;
 		}
-		for (this->m_count = 0; this->m_count < r.m_count; this->m_count++)
+		if constexpr
+			(
+				(std::is_integral_v<T> && std::is_integral_v<U> && sizeof(T) == sizeof(U)) ||
+				(std::is_floating_point_v<T> && std::is_floating_point_v<U> && sizeof(T) == sizeof(U)) ||
+				(std::is_pointer_v<T> && std::is_pointer_v<U>)
+			)
 		{
-			this->m_buffer[this->m_count] = T(r.m_buffer[this->m_count]);
+			//then we don't have to do anything about conversion
+		}
+		else
+		{
+			for (this->m_count = 0; this->m_count < r.m_count; this->m_count++)
+			{
+				this->m_buffer[this->m_count] = T(r.m_buffer[this->m_count]);
+			}
 		}
 		if constexpr (sizeof(T) <= sizeof(U))
 		{
@@ -100,13 +146,52 @@ struct ppvector
 		return this->reserve(this->m_capacity + additional_capacity);
 	}
 
-	template<std::convertible_to<T> U>
+	template<typename U> requires(std::convertible_to<U, T>)
 	bool push_back(const U& value) noexcept
 	{
 		if (!this->reserve_more(1)) return false;
 
 		this->m_buffer[this->m_count++] = T(value);
 		return true;
+	}
+
+	bool shrink() noexcept
+	{
+		if (this->m_capacity > this->m_count)
+		{
+			if ((this->m_buffer = realloc(this->m_buffer, this->m_count * sizeof(T))) == nullptr)
+			{
+				void* new_mem = malloc(this->m_count * sizeof(T));
+				if (new_mem = nullptr) return false;
+
+				memcpy(new_mem, this->m_buffer, this->m_count * sizeof(T));
+				::free(this->m_buffer);
+				this->m_buffer = (T*)new_mem;
+			}
+
+			this->m_capacity = this->m_count;
+			return true;
+		}
+	}
+
+	void free() noexcept
+	{
+		::free(this->m_buffer);
+		this->m_capacity = this->m_count = 0;
+	}
+
+	size_t size() const noexcept
+	{
+		return this->m_count;
+	}
+	size_t count() const noexcept
+	{
+		return this->m_count;
+	}
+
+	size_t capacity() const noexcept
+	{
+		return this->m_capacity;
 	}
 };
 
