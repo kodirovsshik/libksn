505a611078bde14b87fbf7075224c05c0ea18336
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2021-02-23 16:14:07 +0300

    [metapr] ksn::same_to_cv and fundament for ksn::is_compatible

--- ksn/include/ksn/metapr.hpp
+++ ksn/include/ksn/metapr.hpp
@@ -43,6 +43,47 @@ constexpr bool is_any_of_v = is_any_of<T, Args...>::value;
 
 
 
+template<class A, class B>
+struct is_same_to_cv
+	: std::bool_constant< std::is_same_v<std::remove_cv_t<A>, std::remove_cv_t<B>> >
+{
+};
+
+template<class A, class B>
+constexpr bool is_same_to_cv_v = is_same_to_cv<A, B>::value;
+
+template<class T, class U>
+concept same_to_cv = is_same_to_cv_v<T, U>;
+
+
+
+
+
+template<class A, class B>
+struct is_compatible
+	: std::false_type
+{	
+};
+
+template<class A>
+struct is_compatible<A, A>
+	: std::true_type
+{
+};
+
+template<class A, ksn::same_to_cv<A> B>
+struct is_compatible<A, B>
+	: std::true_type
+{
+};
+
+
+template<class A, class B>
+static constexpr bool is_compatible_v = is_compatible<A, B>::value;
+
+
+
+
 
 _KSN_END
 
