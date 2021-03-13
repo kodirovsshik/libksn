7e94abf2141cfd2fe503ef533813249eb135924e
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2021-03-13 15:27:10 +0300

    [try_smol_buffer] Add project files

---
+++ ksn/include/ksn/try_smol_buffer.hpp
@@ -0,0 +1,115 @@
+
+#ifndef _KSN_TRY_SMOL_BUFFER_HPP_
+#define _KSN_TRY_SMOL_BUFFER_HPP_
+
+
+#include <ksn/ksn.hpp>
+
+#include <type_traits>
+//#include <alig>
+
+#include <string.h>
+
+
+
+_KSN_BEGIN
+
+
+template<typename T, size_t N>
+struct try_smol_buffer
+{
+	std::aligned_storage_t< N * sizeof(T), alignof(T)> smol;
+	T* arr;
+	size_t cap;
+	
+	try_smol_buffer() noexcept
+		: arr((T*)&smol), cap(0) {}
+	try_smol_buffer(size_t size) noexcept
+	{
+		this->try_smol_buffer<T, N>::try_smol_buffer();
+
+		this->cap = size;
+		if (size > N)
+		{
+			this->arr = (T*)malloc(size * sizeof(T));
+		}
+	}
+	template<typename T1, size_t N1>
+	try_smol_buffer(const try_smol_buffer<T1, N1>& r) noexcept
+	{
+		this->try_smol_buffer<T, N>::try_smol_buffer(r.cap);
+		memcpy(this->arr, r.arr, sizeof(T) * this->cap);
+	}
+	try_smol_buffer(try_smol_buffer<T, N>&& r) noexcept
+	{
+		this->try_smol_buffer<T, N>::try_smol_buffer();
+
+		if (r.cap > N)
+		{
+			std::swap(this->smol, r.smol);
+		}
+		std::swap(this->arr, r.arr);
+		std::swap(this->cap, r.cap);
+	}
+
+	~try_smol_buffer() noexcept
+	{
+		if (this->cap > N) free(this->arr);
+	}
+
+	void resize(size_t n) noexcept
+	{
+		if (n <= N)
+		{
+			if (this->cap > N)
+			{
+				free(this->arr);
+				this->arr = (T*)&smol;
+			}
+		}
+		else
+		{
+			if (this->cap <= N)
+			{
+				this->arr = (T*)malloc(N * sizeof(T));
+			}
+		}
+		this->cap = n;
+	}
+	void resize_add(size_t add) noexcept
+	{
+		this->resize(this->cap + add);
+	}
+
+	T* data() noexcept
+	{
+		return this->arr;
+	}
+	const T* data() const noexcept
+	{
+		return this->arr;
+	}
+	T& operator[](size_t n) noexcept
+	{
+		return arr[n];
+	}
+	const T& operator[](size_t n) const noexcept
+	{
+		return arr[n];
+	}
+
+	operator T*() noexcept
+	{
+		return this->arr;
+	}
+	operator const T*() const noexcept
+	{
+		return this->arr;
+	}
+};
+
+
+_KSN_END
+
+
+#endif //!_KSN_TRY_SMOL_BUFFER_HPP_
