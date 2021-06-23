6935ccb695ff2ea94cfd471b34e2830efc8a1e00
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2021-06-23 18:08:26 +0300

    Sync

--- temp/temp.cpp
+++ temp/temp.cpp
@@ -1,27 +1,40 @@
+#include <iostream>
+#include <chrono>
+#include <thread>
 
-#include <memory>
-
-
-template<class impl_t>
-class impl_wrapper
-{
-    std::unique_ptr< impl_t> m_impl;
-public:
-    impl_wrapper();
-    ~impl_wrapper();
-};
-
-class wgl
-{
-    class wgl_impl;
-public:
-
-    impl_wrapper<wgl_impl> m_impl;
-};
+#define NOMINMAX
+#include <Windows.h>
 
 int main()
 {
 
-    wgl obj;
+    bool b = std::numeric_limits<uint64_t>::max() > int(-1);
+
+    using namespace std;
+    using namespace chrono;
+    using clock_t = high_resolution_clock;
+
+    auto time_now = clock_t::now();
+    auto time_last = clock_t::now();
+
+    while (true)
+    {
+
+
+        //work
+        Sleep(50);
+
+
+        time_now = clock_t::now();
+
+        auto work_time = duration_cast<milliseconds>(time_now - time_last).count();
+
+        std::this_thread::sleep_for(milliseconds(200 - work_time));
+
+        time_last = clock_t::now();
+        auto sleep_time = duration_cast<milliseconds>(time_last - time_now).count();
+
+        printf("Work: %i, Sleep: %i, Cycle: %i\n", (int)work_time, (int)sleep_time, int(work_time + sleep_time));
+    }
 
 }
