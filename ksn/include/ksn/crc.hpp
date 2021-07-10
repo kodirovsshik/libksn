d28e03a2eaba71bc72c7586597a8e29f9468102e
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2021-07-10 04:02:34 +0300

    CRC: added crc32, updated API

--- ksn/include/ksn/crc.hpp
+++ ksn/include/ksn/crc.hpp
@@ -14,8 +14,13 @@
 _KSN_BEGIN
 
 
+uint32_t crc32_init() noexcept;
+uint32_t crc32_update(const void* data, size_t size, uint32_t previous) noexcept;
+uint32_t crc32(const void* data, size_t size) noexcept;
+
 uint64_t crc64_ecma_init() noexcept;
-uint64_t crc64_ecma(const void* data, size_t size, uint64_t init = crc64_ecma_init()) noexcept;
+uint64_t crc64_ecma_update(const void* data, size_t size, uint64_t previous) noexcept;
+uint64_t crc64_ecma(const void* data, size_t size) noexcept;
 
 
 _KSN_END
