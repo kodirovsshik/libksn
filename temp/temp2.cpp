2b78b237e866e2887150396c67af2ea1b1cfd7a1
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2021-09-08 20:48:42 +0300

    SYnc

--- temp/temp2.cpp
+++ temp/temp2.cpp
@@ -1,26 +1,9 @@
 
-#include <ksn/window.hpp>
-
-#pragma comment(lib, "libksn_window.lib")
-#pragma comment(lib, "libksn_time.lib")
+#include <iostream>
 
 int main()
 {
 
-	ksn::window_t win;
-	win.open(200, 200);
-	win.set_framerate(60);
+	
 
-	while (true)
-	{
-		ksn::event_t ev;
-		while (win.poll_event(ev))
-		{
-
-		}
-
-		win.tick();
-	}
-
-	return 0;
 }
