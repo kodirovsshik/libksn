60c20385984e5a3d8b650edda188713b56e1482e
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2021-09-12 19:04:57 +0300

    Sync

--- temp/temp.cpp
+++ temp/temp.cpp
@@ -1,17 +1,37 @@
 
+#include <iostream>
+#include <iomanip>
 
-constexpr int f()
-{
-	ksn::long_int512_t x(-3.141592);
-	return (int)(float)x;
-}
+#include <ksn/math_long_integer.hpp>
+#include <ksn/time.hpp>
 
+#pragma comment(lib, "libksn_time")
 
 int main()
 {
 
-	constexpr int x = f();
-	f();
+	constexpr int64_t N = 10000000;
+	constexpr size_t limbs = 32;
+
+	ksn::long_int_stack<limbs> x = 0, one = 1;
+
+	for (size_t i = 0; i < limbs / 2; ++i)
+	{
+		x.m_storage.get_storage()[i] = 1;
+	}
+
+	x += one;
+
+
+	ksn::stopwatch sw;
+	sw.start();
+	for (size_t i = 0; i < N; ++i)
+	{
+		x += one;
+	}
+	auto dt = sw.stop();
+
+	std::cout << (int)*std::launder(&x) << ", " << std::setw(10) << dt.as_nsec() << " ns, " << N / dt.as_float_sec() << "/s\n";
 
 	return 0;
 }
